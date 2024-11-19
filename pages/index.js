50ef671dd5c9969cf03f8476321bc7e7af87f496
armandoxd03 <johnroyducut04@gmail.com>
2024-11-19 11:30:12 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,24 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, i'm Johnroy. I am a Software Engineer.</p>
-        <p>10 Things That Require Zero Talent</p>
+        <hr></hr>
+        <p>I am <strong><em>John Roy C. Ducut.</em></strong>  Currently I am a 20 years old student who is at his 3rd year in the department of Information Technology at The University of the Assumption, and I live in the town of Macabebe. My hobbies are reading books, novels, manga, and gaming. My favoritetype of music is pop music. I especially like musicians like Alec Benjamin, Maroon 5, Sabaton, and Zack Tabudlo. My favorite foods are mostly potato dishes, and snacks like fries, hash browns, and mashed potatoes.</p>
+        <hr></hr>
 
-        <ul>
-          <li>Being On Time</li>
-          <li>Making An Effort</li>
-          <li>Being High Energy</li>
-          <li>Having A Positive Attitude</li>
-          <li>Being Passionate</li>
-          <li>Using Good Body Language</li>
-          <li>Being Coachable</li>
-          <li>Doing A Little Extra</li>
-          <li>Being Prepared</li>
-          <li>Having A Strong Work Ethics</li>        
-        </ul>
+        <p>I chose the IT department because, at the time, it was in high demand and aligned with my interests in computers. IT also has many branches on which I can choose from when I work.
+        </p>
+        <hr></hr>
+
+        <p>I’m thinking about becoming a software engineer or a data administrator after my Graduating.
+        </p>
+        <hr></hr>
+        <p>I expect to learn how to connect different software and hardware systems, focusing on making them work together smoothly. I also want to gain hands-on experience with tools for integration, problem-solving, and improving system performance.
+        </p>
+        <hr></hr>
+
+        <p>I’m interested in learning about different ways to integrate systems, using middleware and APIs, and managing data between systems. I’d also like to cover cloud integration, security, real-life examples, and tools for system integration that I will be able to use once I begin working.
+        </p>
+        <hr></hr>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
